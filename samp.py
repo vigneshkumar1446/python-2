9380eb1489bf10fbc8bd7429f3b710666bc8536e
VIGNESH <vigneshkumar1446@gmail.com>
2025-09-17 10:17:36 +0530

    First release of Hello World!

---
+++ samp.py
@@ -0,0 +1,29 @@
+#a= 6
+#b =3
+
+#print(a&b)
+
+a="200"
+
+b=int(a)
+print(type(b))
+print(b)
+
+c=50
+d=str(c)
+print(d)
+print(type(d))
+
+cls="python"
+prg=list(cls)
+
+print(prg)
+
+std=dict(name="vignesh",age=22,course="python")
+print(std)
+
+name=input("enter your name:")
+age=int(input("enter your age:"))
+
+
+print(f"my name is{name},iam {age} year old ")
